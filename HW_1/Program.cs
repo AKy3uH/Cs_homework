619110aa733ead685f80126422bd2c933fbbbbbe
Ky3uH <aakuzin@gmail.com>
2022-12-08 16:06:24 +0300

    2 ready

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,19 +1,38 @@
-﻿Console.WriteLine("Введите 2 числа: ");
+﻿// Console.WriteLine("Введите 2 числа: ");
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// int max = a;
+// if (a==b)
+// {
+//     Console.Write("Вы ввели равные числа");
+// }
+// if (b>max)
+// {
+//     max = b;
+//     Console.WriteLine("число " + b + " большее");
+//     Console.Write("число " + a + " меньшее");
+// }
+// else
+// {
+//     Console.WriteLine("число " + a + " большее");
+//     Console.Write("число " + b + " меньшее");
+// }
+
+Console.WriteLine("Введите 3 числа: ");
 int a = int.Parse(Console.ReadLine());
 int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
 int max = a;
-if (a==b)
-{
-    Console.Write("Вы ввели равные числа");
-}
-if (b>max)
+// if (a == b | b == c | a == c)
+// {
+//     Console.Write("Вы ввели равные числа");
+// }
+if (b > max)
 {
     max = b;
-    Console.WriteLine("число " + b + " большее");
-    Console.Write("число " + a + " меньшее");
 }
-else
+if (c > max)
 {
-    Console.WriteLine("число " + a + " большее");
-    Console.Write("число " + b + " меньшее");
+    max = c;
 }
+Console.WriteLine("число " + max + " наибольшее");
