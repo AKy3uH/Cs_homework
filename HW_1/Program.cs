195eb897ceb00a9ada417f1986eb7b820f84f20c
Ky3uH <aakuzin@gmail.com>
2022-12-08 15:49:15 +0300

    1 ready

---
+++ HW_1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 2 числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = a;
+if (a==b)
+{
+    Console.Write("Вы ввели равные числа");
+}
+if (b>max)
+{
+    max = b;
+    Console.WriteLine("число " + b + " большее");
+    Console.Write("число " + a + " меньшее");
+}
+else
+{
+    Console.WriteLine("число " + a + " большее");
+    Console.Write("число " + b + " меньшее");
+}
