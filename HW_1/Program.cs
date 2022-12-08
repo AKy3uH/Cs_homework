8a8335d60853d0dbc837648963231f70b60129c7
Ky3uH <aakuzin@gmail.com>
2022-12-08 17:19:11 +0300

    4 ready

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,4 +1,6 @@
-﻿// Console.WriteLine("Введите 2 числа: ");
+﻿////на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите 2 числа: ");
 // int a = int.Parse(Console.ReadLine());
 // int b = int.Parse(Console.ReadLine());
 // int max = a;
@@ -19,6 +21,7 @@
 // }
 
 
+////принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 // Console.WriteLine("Введите 3 числа: ");
 // int a = int.Parse(Console.ReadLine());
@@ -40,15 +43,33 @@
 // Console.WriteLine("число " + max + " наибольшее");
 
 
+////на вход принимает число и выдаёт, является ли число чётным.
 
-Console.WriteLine("Введите число:");
-int a = int.Parse(Console.ReadLine());
-a = a % 2;
-if (a == 0)
-{
-    Console.WriteLine("Вы ввели чётное число");
-}
-else
-{
-    Console.WriteLine("Вы ввели НЕчётное число");
-}
+// Console.WriteLine("Введите число:");
+// int a = int.Parse(Console.ReadLine());
+// a = a % 2;
+// if (a == 0)
+// {
+//     Console.WriteLine("Вы ввели чётное число");
+// }
+// else
+// {
+//     Console.WriteLine("Вы ввели НЕчётное число");
+// }
+
+
+////на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Введите число (больше 2):");
+// int N = int.Parse(Console.ReadLine());
+// if (N>2)
+// {
+//     for (int num = 2; num < N; num += 2)
+//     {
+//         Console.Write(" " + num);
+//     }
+// }
+// else
+// {
+//     Console.Write("Вы ввели число 2 или меньше.");
+// }
